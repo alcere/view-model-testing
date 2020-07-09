b5287b287616428c9507e400ef92382d6cbe4249
alfredo <alcere@microsoft.com>
2020-07-09 12:01:37 +0200

    remove redundant list

--- app/src/test/java/utils/LiveDataTestExtensions.kt
+++ app/src/test/java/utils/LiveDataTestExtensions.kt
@@ -3,16 +3,15 @@ package utils
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
 
-fun <T> LiveData<T>.observeForTesting(block: (ValuesObserver<T>) -> Unit) =
-    mutableListOf<T>().apply {
-        val observer = ValuesObserver<T>()
+fun <T> LiveData<T>.observeForTesting(block: (ValuesObserver<T>) -> Unit) {
+     val observer = ValuesObserver<T>()
         try {
             observeForever(observer)
             block(observer)
         } finally {
             removeObserver(observer)
         }
-    }
+}
 
 class ValuesObserver<T> : Observer<T> {
     val values = mutableListOf<T>()
